44b59889db254b087a5c4d8dfb25cc1eefa9d648
Steve Vuillaume <steve.vuillaume@divcom.ch>
2019-05-20 11:46:31 +0200

    Création 1er plugin

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -65,9 +65,20 @@ function mon_plugin_yoda_shortcode() {
     return "<blockquote>Que la force soit avec toi jeune padawan !</blockquote>";
 }
 
+function mon_plugin_iso_shortcode($atts, $content = "") {
+
+
+      src="https://esig-ju.ch/b73/svu/voyages/wp-content/plugins/ico-plugin/img/wordpress.png";
+
+    return src;
+}
+
 //Enregistre les shortcodes du plugin
 function mon_plugin_register_shortcode() {
     add_shortcode( 'yoda', 'mon_plugin_yoda_shortcode' );
     add_shortcode( 'vador', 'mon_plugin_vador_shortcode' );
+    add_shortcode( 'iso', 'mon_plugin_iso_shortcode' );
 }
 add_action( 'init', 'mon_plugin_register_shortcode' );
+
+
