bee0cd13f91ca47a6c1d29ff241a2e1691ff632c
Steve Vuillaume <steve.vuillaume@divcom.ch>
2019-05-20 09:54:46 +0200

    Création 1er plugin

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -3,3 +3,9 @@
 Plugin Name: Mon Premier Plugin
  */
 
+//Fonction qui affiche la balise meta
+function mon_plugin_meta_keywords() {
+    echo '<meta name="keywords" content="HTML,CSS,XML,JavaScript">';
+}
+//Ajout d'une action sur 'wp_head' qui appellera mon_plugin_meta_keywords()
+add_action('wp_head', 'mon_plugin_meta_keywords' );
